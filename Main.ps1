8c29d83f2561506fe8b77a2e9463acac33fdfd06
HalfDemon <monican82@gmail.com>
2016-05-12 15:20:20 -0500

    Converted some code, added a function
    Converted the java function to more PowerShell like. Added a quick
    selection to the Install-Java Function to select between single or
    multiple machines and if multiple to ask for the file to pull the
    hostnames from.

--- Main.ps1
+++ Main.ps1
@@ -18,7 +18,7 @@ $type=$args[2]
 function Check
 {
 	[CmdletBinding()]
-	param ()
+	param()
 	$system = Read-Host -Prompt 'Destination System:'
 	Write-Host If Output is null or empty please check hostname and try again
 	Get-ADComputer -filter { name -like $system } | select DistinguishedName, Enabled, DNSHostName | Format-List
@@ -36,7 +36,7 @@ function Check
 function Install-Baseline
 {
 	[CmdletBinding()]
-	param ()
+	param()
 	$system = Read-Host -Prompt 'Destination System:'
 	set-location 'c:\staging\'
 	write-host ("Copying Baseline to $system ")
@@ -74,7 +74,32 @@ function Get-UserSize
 function Install-Java
 {
 	[CmdletBinding()]
+	param
+	(
+		$PMulti
+	)
+	$major = Read-Host -Prompt 'Java Major Revision:'
+	$minor = Read-Host -Prompt 'Java Minor Revision'
+	
+	[int]$xMenuChoiceA = 0
+	while ($xMenuChoiceA -lt 1 -or $xMenuChoiceA -gt 2)
+	{
+		Write-host "1. Single"
+		Write-host "2. Multi"
+		[Int]$xMenuChoiceA = read-host "Please enter an option 1 or 2"
+	}
+	Switch ($xMenuChoiceA)
+	{
+		1{Java-Single}
+		2{Java-Multi}
+		default {Java-Single}
+	}
+}
+function Java-Single
+{
+	[CmdletBinding()]
 	param ()
+	
 	$system = Read-Host -Prompt 'Destination System:'
 	$major = Read-Host -Prompt 'Java Major Revision:'
 	$minor = Read-Host -Prompt 'Java Minor Revision'
@@ -85,4 +110,21 @@ function Install-Java
 	.\psexec.exe \\$system -h -s c:\staged\java\jre$major-$minor-x86.exe /s
 	.\psexec.exe \\$system -h -s c:\staged\java\jre$major-$minor-x64.exe /s
 	echo f | .\psexec \\$system -h -s reg import "c:\staged\java\java.reg"
+}
+function Java-Multi
+{
+	[CmdletBinding()]
+	param ()
+	Write-Host 'Multiple Hosts will be targeted'
+	$jMultiFile = Read-Host -Prompt 'File with hostnames:'
+	foreach ($jHostname in Get-Content $jMultiFile )
+	{
+		set-location 'c:\staging\'
+		write-host Java
+		xcopy java ("\\$jHostname\C$\staged\Java\") /e /c /h /y /z
+		set-location 'c:\scripts\'
+		.\psexec.exe \\$jHostname -h -s c:\staged\java\jre$major-$minor-x86.exe /s
+		.\psexec.exe \\$jHostname -h -s c:\staged\java\jre$major-$minor-x64.exe /s
+		echo f | .\psexec \\$jHostname -h -s reg import "c:\staged\java\java.reg"
+	}
 }
